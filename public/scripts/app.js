98a0c49c407e4db90d107a944f0c86cffdf884e7
AndresHaayen <andreshaayen@gmail.com>
2017-11-20 00:37:21 +0100

    Loaded in React and ReactDOM.

---
+++ public/scripts/app.js
@@ -0,0 +1,14 @@
+console.log('App.js is running!');
+
+// JSX is a JavaScript Syntax Extention
+// JSX - JavaScript XML
+// var template = <p>This is JSX from app.js!</p>;
+var template = React.createElement(
+    "h1",
+    { id: "someid"},
+    "Something New"
+);
+var appRoot = document.getElementById('app');
+
+
+ReactDOM.render(template, appRoot);
