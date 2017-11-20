3867d08984cabd696b82ba596fc972e9b6577260
AndresHaayen <andreshaayen@gmail.com>
2017-11-20 20:18:18 +0100

    added babel and scripts to package.json

--- src/app.js
+++ src/app.js
@@ -1,15 +1,9 @@
-'use strict';
-
 console.log('App.js is running!');
 
 // JSX is a JavaScript Syntax Extention
 // JSX - JavaScript XML
 
-var template = React.createElement(
-  'h1',
-  null,
-  'Indecision App'
-);
+var template = <h1>Indecision App</h1>;
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
