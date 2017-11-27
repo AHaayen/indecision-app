cefc2dc092170c43ad60cdbec96a2a97824dd79f
AndresHaayen <andreshaayen@gmail.com>
2017-11-27 18:02:25 +0100

    ES6 Classes: Part I

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,100 +1,38 @@
 'use strict';
 
-console.log('App.js is running!');
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-// JSX is a JavaScript Syntax Extention
-// JSX - JavaScript XML
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-var app = {
-    title: 'Indecision App',
-    subtitle: 'The computer was born to solve problems that did not exist before',
-    options: []
-};
+var Person = function () {
+    function Person() {
+        var name = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'Anonymous';
+        var age = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 0;
 
-var onFormSubmit = function onFormSubmit(e) {
-    e.preventDefault();
+        _classCallCheck(this, Person);
 
-    var option = e.target.elements.option.value;
-
-    if (option) {
-        app.options.push(option);
-        e.target.elements.option.value = '';
-        render();
+        this.name = name;
+        this.age = age;
     }
-};
 
-var deleteElement = function deleteElement() {
-    app.options = [];
-    render();
-};
+    _createClass(Person, [{
+        key: 'getGretting',
+        value: function getGretting() {
+            // return 'Hi. I am ' + this.name + '!';
+            return 'Hi. I am ' + this.name + '!';
+        }
+    }, {
+        key: 'getDescription',
+        value: function getDescription() {
+            return this.name + ' is ' + this.age + ' year(s) old.';
+        }
+    }]);
 
-var onMakeDecision = function onMakeDecision() {
-    var randomNum = Math.floor(Math.random() * app.options.length);
-    var option = app.options[randomNum];
-    alert(option);
-};
+    return Person;
+}();
 
-var appRoot = document.getElementById('app');
+var me = new Person('Andres Haayen', 23);
+console.log(me.getDescription());
 
-var numbers = [55, 101, 1000];
-
-var render = function render() {
-    var template = React.createElement(
-        'div',
-        null,
-        React.createElement(
-            'h1',
-            null,
-            app.title
-        ),
-        app.subtitle && React.createElement(
-            'p',
-            null,
-            app.subtitle
-        ),
-        React.createElement(
-            'p',
-            null,
-            app.options.length > 0 ? 'Here are your options' : 'No Options'
-        ),
-        React.createElement(
-            'p',
-            null,
-            app.options.length
-        ),
-        React.createElement(
-            'button',
-            { disabled: app.options.length === 0, onClick: onMakeDecision },
-            'What should I do'
-        ),
-        React.createElement(
-            'button',
-            { onClick: deleteElement },
-            'Remove All'
-        ),
-        React.createElement(
-            'ol',
-            null,
-            app.options.map(function (option) {
-                return React.createElement(
-                    'li',
-                    { key: option },
-                    option
-                );
-            })
-        ),
-        React.createElement(
-            'form',
-            { onSubmit: onFormSubmit },
-            React.createElement('input', { type: 'text', name: 'option' }),
-            React.createElement(
-                'button',
-                null,
-                'Add Option'
-            )
-        )
-    );
-    ReactDOM.render(template, appRoot);
-};
-
-render();
+var other = new Person();
+console.log(other.getDescription());
--- src/playground/build-it-visible.js
+++ src/playground/build-it-visible.js
@@ -1,31 +1,66 @@
+// const app = {
+//     title: 'Visibility Toggle'
+// }
+
+// let visibility = false;
+
+// const toggleVisibility = () => {
+//     visibility = !visibility;
+//     render();
+// };
+
+
+// const render = () => {
+//     const build = (
+//         <div>
+//             <h1>{app.title}</h1>
+//             <button onClick={toggleVisibility}>
+//                 {visibility ? 'Hide Details' : 'Show Details'}
+//             </button>
+//             {visibility && (
+//                 <div>
+//                     <p>Here are some details you can see!</p>
+//                 </div>
+//             )}
+//         </div>
+//     );
+
+//     ReactDOM.render(build, document.getElementById('app'));
+// };
+
+// render();
+
+
+
 const app = {
-    title: 'Visibility Toggle'
+    title: "Visibility Toggle"
 }
 
 let visibility = false;
 
-const toggleVisibility = () => {
-    visibility = !visibility;
+const theVisibility = () => {
+   visibility = !visibility;
     render();
 };
-
 
 const render = () => {
     const build = (
         <div>
             <h1>{app.title}</h1>
-            <button onClick={toggleVisibility}>
+            <button onClick={theVisibility}>
                 {visibility ? 'Hide Details' : 'Show Details'}
             </button>
             {visibility && (
                 <div>
-                    <p>Here are some details you can see!</p>
+                    <p>Here are the details</p>
                 </div>
             )}
-        </div>
-    );
+        </div> 
+    )
 
     ReactDOM.render(build, document.getElementById('app'));
+    
+
 };
 
 render();
