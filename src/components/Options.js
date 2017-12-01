c94b7a1b750a92a2066c9a1c2a99f3200b7eca3a
AndresHaayen <andreshaayen@gmail.com>
2017-12-01 17:19:37 +0100

    One Component per File

---
+++ src/components/Options.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import Option from './Option.js';
+
+const Options = (props) => {
+    return (
+        <div>
+            <button onClick={props.handleDeleteOptions}>Remove All</button>
+            {props.options.length === 0 && <p>Please add an option to get started</p>}
+            {
+                props.options.map((options) => (
+                    <Option
+                        key={options}
+                        optionText={options}
+                        handleDeleteOption={props.handleDeleteOption}
+                    />
+                ))
+            }
+        </div>
+    );
+};
+
+export default Options;
+
+
+
+
+
+
+
+// class Options extends React.Component {
+//     render() {
+//         return (
+//             <div>
+//                 <button onClick={this.props.handleDeleteOptions}>Remove All</button>
+//                 {
+//                     this.props.options.map((options) => <Option key={options} optionText={options} />)
+//                 }
+//             </div>
+//         );
+//     }
+// }
