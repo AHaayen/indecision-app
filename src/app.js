013701d07141fee8c11fdec564f875a1d4b87cd6
AndresHaayen <andreshaayen@gmail.com>
2017-12-01 16:34:43 +0100

    Importing npm Modules

--- src/app.js
+++ src/app.js
@@ -1,3 +1,30 @@
+// install -> import -> use
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const template = React.createElement('p', {} , 'testing 123');
+ReactDOM.render(template, document.getElementById('app'));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // import './utils.js'
 // import minus, { divide, square, add } from './utils.js'
 
@@ -8,8 +35,8 @@
 // console.log(minus(3, 10));
 // console.log(divide(2, 324));
 
-import isSenior, {isAdult, canDrink} from './person.js'
+// import isSenior, {isAdult, canDrink} from './person.js'
 
-console.log(isAdult(19));
-console.log(canDrink(23));
-console.log(isSenior(64));
+// console.log(isAdult(19));
+// console.log(canDrink(23));
+// console.log(isSenior(64));
