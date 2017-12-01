f250643d5c9dc2a38ddca2b7f1af16f081c60578
AndresHaayen <andreshaayen@gmail.com>
2017-12-01 16:49:25 +0100

    Made a different folder for Import/Export for Recap and Practice

--- public/bundle.js
+++ public/bundle.js
@@ -977,22 +977,6 @@ var template = _react2.default.createElement(
 );
 _reactDom2.default.render(template, document.getElementById('app'));
 
-// import './utils.js'
-// import minus, { divide, square, add } from './utils.js'
-
-
-// console.log('app.js is running!');
-// console.log(square(4));
-// console.log(add(3, 4));
-// console.log(minus(3, 10));
-// console.log(divide(2, 324));
-
-// import isSenior, {isAdult, canDrink} from './person.js'
-
-// console.log(isAdult(19));
-// console.log(canDrink(23));
-// console.log(isSenior(64));
-
 /***/ }),
 /* 16 */
 /***/ (function(module, exports, __webpack_require__) {
--- src/app.js
+++ src/app.js
@@ -5,39 +5,3 @@ import ReactDOM from 'react-dom';
 const template = <p>This is JSX from Webpack</p>;
 ReactDOM.render(template, document.getElementById('app'));
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import './utils.js'
-// import minus, { divide, square, add } from './utils.js'
-
-
-// console.log('app.js is running!');
-// console.log(square(4));
-// console.log(add(3, 4));
-// console.log(minus(3, 10));
-// console.log(divide(2, 324));
-
-// import isSenior, {isAdult, canDrink} from './person.js'
-
-// console.log(isAdult(19));
-// console.log(canDrink(23));
-// console.log(isSenior(64));
