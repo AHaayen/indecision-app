111b73efd4b547ad6cdf8b7df02f2cd05f5713e0
AndresHaayen <andreshaayen@gmail.com>
2017-12-01 15:59:35 +0100

    Learned how to import and export.

--- src/app.js
+++ src/app.js
@@ -1,1 +1,13 @@
-console.log('app.js is running!');
+// import './utils.js'
+// import { square, add, minus } from './utils.js'
+
+
+// console.log('app.js is running!');
+// console.log(square(4));
+// console.log(add(3, 4));
+// console.log(minus(3, 4));
+
+import {isAdult, canDrink} from './person.js'
+
+console.log(isAdult(19));
+console.log(canDrink(23));
---
+++ src/person.js
@@ -0,0 +1,20 @@
+// export const isAdult = (age) => {
+//     if(age >= 18){
+//         return true;
+//     }
+//     return false
+// }
+
+// export const canDrink = (age) => {
+//     if(age >= 21){
+//         return true;
+//     }
+//     return false
+// }
+
+// ---------------------------------
+
+const isAdult = (age) => age >= 18;
+const canDrink = (age) => age >= 21;
+
+export {isAdult, canDrink};
