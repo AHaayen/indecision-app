857530ddd59676ee2380eab18074b919097913d5
AndresHaayen <andreshaayen@gmail.com>
2017-12-04 23:27:57 +0100

    bIG Button & Options List

--- src/components/Options.js
+++ src/components/Options.js
@@ -2,20 +2,25 @@ import React from 'react';
 import Option from './Option.js';
 
 const Options = (props) => (
-        <div>
-            <button onClick={props.handleDeleteOptions}>Remove All</button>
-            {props.options.length === 0 && <p>Please add an option to get started</p>}
-            {
-                props.options.map((options) => (
-                    <Option
-                        key={options}
-                        optionText={options}
-                        handleDeleteOption={props.handleDeleteOption}
-                    />
-                ))
-            }
-        </div>
-    );
+    <div>
+        <button
+            className="button button--link"
+            onClick={props.handleDeleteOptions}
+        >
+            Remove All
+            </button>
+        {props.options.length === 0 && <p>Please add an option to get started</p>}
+        {
+            props.options.map((options) => (
+                <Option
+                    key={options}
+                    optionText={options}
+                    handleDeleteOption={props.handleDeleteOption}
+                />
+            ))
+        }
+    </div>
+);
 
 export default Options;
 
