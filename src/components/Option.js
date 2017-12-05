5290fbc546e19b1ac50969022cdd669156332ae0
AndresHaayen <andreshaayen@gmail.com>
2017-12-05 19:36:15 +0100

    Learned how to Style React Modal.

--- src/components/Option.js
+++ src/components/Option.js
@@ -1,18 +1,18 @@
 import React from 'react';
 
 const Option = (props) => (
-        <div className="option">
+    <div className="option">
         <p className="option__text">{props.count}. {props.optionText} </p>
-            <button
-                className="button button--link"
-                onClick={(e) => {
-                    props.handleDeleteOption(props.optionText);
-                }}
-            >
-                remove
+        <button
+            className="button button--link"
+            onClick={(e) => {
+                props.handleDeleteOption(props.optionText);
+            }}
+        >
+            remove
             </button>
-        </div>
-    );
+    </div>
+);
 
 export default Option;
 
